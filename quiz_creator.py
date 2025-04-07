d6838ea5bd451e5fe301a2badc709af4efd4718f
Jacuzzi <jheklayag@gmail.com>
2025-04-07 23:28:40 +0800

    User input for questions have been added

--- quiz_creator.py
+++ quiz_creator.py
@@ -1,3 +1,5 @@
 # Ask the user a question
+while True:
+    question = input("Enter a question: ")
 # Ask the user for an answer and the correct answer
 # Loop until the user wants to exit the program
