15733fe0a54bc41c460665d2a8ae78f4c6e00ec8
Jacuzzi <jheklayag@gmail.com>
2025-04-07 23:36:45 +0800

    User input for the correct answer has been added

--- quiz_creator.py
+++ quiz_creator.py
@@ -2,4 +2,10 @@
 while True:
     question = input("Enter a question: ")
 # Ask the user for an answer and the correct answer
+    choice_a = input("Enter the answer as choice a: ")
+    choice_b = input("Enter the answer as choice b: ")
+    choice_c = input("Enter the answer as choice c: ")
+    choice_d = input("Enter the answer as choice d: ")
+    
+    correct_answer = input("Enter which of the choices is the correct answer: ").lower()
 # Loop until the user wants to exit the program
