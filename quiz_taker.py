288c89fe8da0ff729b2d02ebc932da8307c9a9cc
Jacuzzi <jheklayag@gmail.com>
2025-05-04 02:17:57 +0800

    Pseudocode

---
+++ quiz_taker.py
@@ -0,0 +1,6 @@
+# Open and read the file
+# Split the file content by question to separate each question block
+# Shuffle the questions to randomize the order
+# Loop through questions and ask for the user's answers
+# Validate if answers are correct
+# Show the final score
